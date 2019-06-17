34156bb9948d3d5b1437324442baa4cc26f8cf25
wodrow-ys-pc <wodrow451611cv@gmail.com>
2019-06-17 18:28:42 +0800

    add bootstrap-vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="home">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6">123</div>
+        <div class="col-lg-6">123</div>
+      </div>
+    </div>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>

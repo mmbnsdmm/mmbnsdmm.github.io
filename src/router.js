1ae09780826733642ef161838c96bda393914ac0
wodrow-ys-pc <wodrow451611cv@gmail.com>
2019-06-20 15:13:02 +0800

    update route

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// import Lodash from 'lodash'
-// import store from './store'
+import Lodash from 'lodash'
+import store from './store'
 
 Vue.use(Router);
 
@@ -16,8 +16,14 @@ let routes = [
         path: '/about',
         name: 'about',
         meta: {title:'关于-续吧',keywords:'续吧 故事汇 YY故事汇',description:'续吧'},
-        component: () => import(/* webpackChunkName: "about" */ './views/site/About.vue'),
-    }
+        component: () => import( './views/site/About.vue'),
+    },
+    {
+        path: '/login',
+        name: 'login',
+        meta: {title:'登录-续吧',keywords:'续吧 故事汇 YY故事汇',description:'续吧'},
+        component: () => import('./views/site/Login.vue'),
+    },
 ];
 
 const r = new Router({
@@ -26,6 +32,15 @@ const r = new Router({
 
 r.beforeEach(function (to, from, next) {
     document.title = to.meta.title;
+    let user = store.state.user;
+    let no_need_login_routes = ['/login', '/signup'];
+    if (!user.id){
+        Lodash.forEach(no_need_login_routes ,function (v) {
+            if (v !== to.path){
+                next('/login');
+            }
+        })
+    }
     next()
 });
 

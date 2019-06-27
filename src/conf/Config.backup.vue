09b37bb3b9d6a3d2d3ff459430ada53e098887c6
wodrow-ys-pc <wodrow451611cv@gmail.com>
2019-06-27 18:19:19 +0800

    update

---
+++ src/conf/Config.backup.vue
@@ -0,0 +1,7 @@
+<script>
+export default {
+    name: "Config",
+    // api_base_url: "http://xuba2.ys.pc/vueappapi/index-dev.php"
+    api_base_url: "http://120.92.150.43:8006/vueappapi/index-dev.php"
+}
+</script>

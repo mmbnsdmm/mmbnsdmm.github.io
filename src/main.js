4044ead92bccd9899b606df548cdcd84aa096377
wodrow-home <wodrow451611cv@gmail.com>
2019-06-27 21:38:00 +0800

    update

--- src/main.js
+++ src/main.js
@@ -2,10 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import router from './router'
-import Lodash from 'lodash'
-import Axios from 'axios'
-import VeeValidate from 'vee-validate'
-import BootstrapVue from 'bootstrap-vue'
+import axios from 'axios'
+import './validate'
+import VueAxios from 'vue-axios'
 import MintUI from 'mint-ui'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -13,24 +12,21 @@ import 'mint-ui/lib/style.css'
 import 'vue-icon-font'
 
 import Tools from '@/components/wodrow/Tools'
+import Config from '@/conf/Config'
+
+axios.defaults.baseURL = Config.api_base_url;
 
 Vue.config.productionTip = false;
 
-Vue.use(VeeValidate);
+Vue.prototype.tools = Tools;
+
 Vue.use(MintUI);
 Vue.use(Tools);
-
-Vue.prototype.Tools = Tools;
+Vue.use(VueAxios, axios);
 
 new Vue({
     router,
     store,
-    Lodash,
-    Axios,
-    VeeValidate,
-    BootstrapVue,
-    MintUI,
-    Tools,
     render: function(h){
         return h(App);
     }
